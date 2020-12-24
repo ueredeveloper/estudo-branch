ec9294c95a05ee5e96c84d9e95fdf1c40c77410e
ueredeveloper <ueredeveloper@gmail.com>
2020-12-24 11:09:27 -0300

    branch-class-student

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
+import models.Student;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("commit one");
+
+		Student st = new Student("José", "Rua dos Pinheiros");
+		
+		System.out.println("Nome: " + st.getName() + " Endere: " + st.getAddress());
 
 	}
 
---
+++ src/models/Student.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Student {
+
+	String name;
+	String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Student(String name, String address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+
+}
